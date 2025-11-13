94fb24ecd10fe0c2b021616bd24791ebe6f5d409
csqr-app <159860489+csqr-app@users.noreply.github.com>
2025-11-13 21:40:28 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,13 +9,6 @@ import react from '@astrojs/react';
 export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
-      // Allow ngrok host for external tunneling during development
-      server: {
-        // Add any hosts you want to allow here (addresses from tunneling services)
-        allowedHosts: [
-          'unsmugly-selenographical-eugenia.ngrok-free.dev'
-        ],
-      },
 	},
 
   integrations: [react()],
